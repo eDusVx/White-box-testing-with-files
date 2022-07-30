e96249e07eccba42052e1e749ae971bb2e0f1ea5
eDusVx <eduardosvx@Gmail.com>
2022-07-30 15:52:18 -0300

    criando novos casos de teste

--- testa_juncao.py
+++ testa_juncao.py
@@ -30,3 +30,33 @@ def teste_caminho_2():
     juncao.juntar()
     with open('3.txt', 'r', encoding="utf-8") as file3:
         assert file3.read().splitlines() == ['1']
+
+
+def teste_caminho_3():
+    """testanto caminho ABCEFID onde o primeiro arquivo está vazio e o segundo tem 1 linha"""
+    arquivo1 = []
+    arquivo2 = ['1']
+    with open('1.txt', 'w', encoding="utf-8") as file1:
+        for item in arquivo1:
+            file1.write(f'{item}\n')
+    with open('2.txt', 'w', encoding="utf-8") as file1:
+        for item in arquivo2:
+            file1.write(f'{item}\n')
+    juncao.juntar()
+    with open('3.txt', 'r', encoding="utf-8") as file3:
+        assert file3.read().splitlines() == ['1']
+
+
+def teste_caminho_4():
+    """testanto caminho ABCEFHD onde os dois arquivos tem 1 linha e registros iguais"""
+    arquivo1 = ['1']
+    arquivo2 = ['1']
+    with open('1.txt', 'w', encoding="utf-8") as file1:
+        for item in arquivo1:
+            file1.write(f'{item}\n')
+    with open('2.txt', 'w', encoding="utf-8") as file1:
+        for item in arquivo2:
+            file1.write(f'{item}\n')
+    juncao.juntar()
+    with open('3.txt', 'r', encoding="utf-8") as file3:
+        assert file3.read().splitlines() == ['1','1']
