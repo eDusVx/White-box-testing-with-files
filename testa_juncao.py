74d62d6336cf40df02ea75ca80207fdfa2b187e7
eDusVx <eduardosvx@Gmail.com>
2022-07-30 17:17:07 -0300

    criando novos casos de teste

--- testa_juncao.py
+++ testa_juncao.py
@@ -59,4 +59,26 @@ def teste_caminho_4():
             file1.write(f'{item}\n')
     juncao.juntar()
     with open('3.txt', 'r', encoding="utf-8") as file3:
-        assert file3.read().splitlines() == ['1','1']
+        assert file3.read().splitlines() == ['1', '1']
+
+
+def teste_caminho_5():
+    """testanto caminho ABCEFHD onde os dois arquivos tem 1 linha e registros iguais"""
+    arquivo1 = ['2', '3', '4', '6', '7', '8', '9']
+    arquivo2 = ['1', '3', '5']
+    with open('1.txt', 'w', encoding="utf-8") as file1:
+        for item in arquivo1:
+            file1.write(f'{item}\n')
+    with open('2.txt', 'w', encoding="utf-8") as file1:
+        for item in arquivo2:
+            file1.write(f'{item}\n')
+    juncao.juntar()
+    with open('3.txt', 'r', encoding="utf-8") as file3:
+        assert file3.read().splitlines() == ['1', '2', '3', '3', '4', '5', '6', '7', '8', '9']
+
+
+teste_caminho_1()
+teste_caminho_2()
+teste_caminho_3()
+teste_caminho_4()
+teste_caminho_5()
